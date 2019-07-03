2e539b207c33f9ba1eee91756e08d10125ad955f
EricTTFO <34400777+EricTTFO@users.noreply.github.com>
2019-07-03 11:59:11 +0800

    S

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="">
-    Ant Design Vue Pro
-    <br />
-    作者：Abel
+    Abel
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style></style>
